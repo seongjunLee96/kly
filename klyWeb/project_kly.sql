923d6760b58ffe3e62c59727e8caeca8e636121b
danny <gryb809@gmail.com>
2018-07-26 14:00:44 +0900

    0.14.1 DB 테이블 정리 및 관련 DTO 생성

--- klyWeb/project_kly.sql
+++ klyWeb/project_kly.sql
@@ -18,6 +18,7 @@ CREATE TABLE BOARD(
 	BOARD_URL NVARCHAR2(200),
 	BOARD_READCOUNT NUMBER DEFAULT 0,
 	BOARD_LIKECOUNT NUMBER DEFAULT 0,
+	BOARD_BLIND NUMBER DEFAULT 0,
 	BOARD_TAG NVARCHAR2(50),
 	BOARD_CATEGORY NVARCHAR2(50) NOT NULL,
 	PRIMARY KEY(BOARD_NUM),
@@ -25,7 +26,7 @@ CREATE TABLE BOARD(
 	REFERENCES MEMBER(MEMBER_ID)
 );
 
-CREATE TABLE BOARD_COMMENT ( --COMMENT라는 테이블 생성 불가
+CREATE TABLE BOARD_COMMENT (
 	BOARD_NUM NUMBER NOT NULL,
 	MEMBER_ID NVARCHAR2(50) NOT NULL,
 	COMMENT_NUM NUMBER NOT NULL,
@@ -51,32 +52,30 @@ CREATE TABLE LIKED(
 );
 
 CREATE TABLE REPORT(
+	MEMBER_ID NVARCHAR2(50) NOT NULL,
+	BOARD_NUM NUMBER NOT NULL,
 	REPORT_NUM NUMBER NOT NULL,
-	MEMBER_ID NVARCHAR2(50) NOT NULL, -- 신고한 사람
-	BOARD_NUM NUMBER NOT NULL, -- 신고한 게시물
-	REPORT_COUNT NUMBER DEFAULT 0, -- 신고 수 , 차단 2개 이상
-	REPORT_DATE DATE, -- 신고시간
+	REPORT_COUNT NUMBER DEFAULT 0,
+	REPORT_DATE DATE,
 	PRIMARY KEY(REPORT_NUM),
 	CONSTRAINT FK_REPORT_MEMBER FOREIGN KEY(MEMBER_ID)
 	REFERENCES MEMBER(MEMBER_ID),
 	CONSTRAINT FK_REPORT_ARTICLE FOREIGN KEY(BOARD_NUM)
 	REFERENCES BOARD(BOARD_NUM)
-);*/
+);
 
--- QUICK
-SELECT * FROM MEMBER;
-DROP TABLE BOARD;
-DROP TABLE MEMBER;
-
--- Dummy Data MEMBER (5)
-INSERT INTO MEMBER VALUES('A','1234','koxk@naver.com',0,Sysdate,0);
+-- Dummy Data MEMBER
+INSERT INTO MEMBER VALUES('123','123','koxk@naver.com',0,Sysdate,0);
+INSERT INTO MEMBER VALUES('qwe','123','dwqwe2@gmail.com',1,sysdate,0);
+INSERT INTO MEMBER VALUES('admin','123','dwqwe2@gmail.com',1,sysdate,0);
+INSERT INTO MEMBER VALUES('tempId','123','dwqwe2@gmail.com',1,sysdate,0);
 INSERT INTO MEMBER VALUES('B','1234','dwqwe2@gmail.com',0,sysdate,0);
 INSERT INTO MEMBER VALUES('C','1234','Jony123@gmail.com',0,sysdate,0);
 INSERT INTO MEMBER VALUES('D','1234','dfdsfw@gmail.com',0,sysdate,0);
 INSERT INTO MEMBER VALUES('E','1234','whdwrkdgu@daum.net',0,sysdate,0);
 
--- Dummy Data BOARD (10)
-INSERT INTO BOARD VALUES('A',1,'aA',Sysdate,null,'<iframe width="420" height="720" src="https://www.youtube.com/embed/zjwqIyoWV8c" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',0,0,0,'롤','게임');
+-- Dummy Data BOARD
+INSERT INTO BOARD VALUES('B',1,'aA',Sysdate,null,'<iframe width="420" height="720" src="https://www.youtube.com/embed/zjwqIyoWV8c" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',0,0,0,'롤','게임');
 INSERT INTO BOARD VALUES('B',2,'aB',Sysdate,null,'<iframe width="420" height="720" src="https://www.youtube.com/embed/sB-yuZ2Qncg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',0,0,0,'배그','게임');
 INSERT INTO BOARD VALUES('C',3,'aC',Sysdate,null,'<iframe width="420" height="720" src="https://www.youtube.com/embed/1yDYGOFc5qY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',0,0,0,'일상','유머');
 INSERT INTO BOARD VALUES('D',4,'aD',Sysdate,null,'<iframe width="420" height="720" src="https://www.youtube.com/embed/9Xlc_UyjQQY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',0,0,0,'축구','스포츠');
@@ -89,7 +88,7 @@ INSERT INTO BOARD VALUES('qwe',10,'aD',Sysdate,null,'<iframe width="420" height=
 INSERT INTO BOARD VALUES('qwe',11,'aE',Sysdate,null,'<iframe width="420" height="720" src="https://www.youtube.com/embed/f3P_lHInI14" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',0,0,0,'개','동물');
 INSERT INTO BOARD VALUES('qwe',12,'aE',Sysdate,null,'<iframe width="420" height="720" src="https://www.youtube.com/embed/f3P_lHInI14" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',0,0,0,'개','동물');
 
--- Dummy Data COMMENT(REPLY) (5)
+-- Dummy Data COMMENT
 INSERT INTO BOARD_COMMENT VALUES(1,'tempId',1,'안녕하세요',SYSDATE,0);
 INSERT INTO BOARD_COMMENT VALUES(1,'tempId',2,'안녕하세요',SYSDATE,0);
 INSERT INTO BOARD_COMMENT VALUES(1,'tempId',3,'안녕하세요',SYSDATE,0);
@@ -102,8 +101,30 @@ INSERT INTO BOARD_COMMENT VALUES(1,'qwe',9,'ㅋㅋㅋㅋㅋㅋadkj',SYSDATE,0);
 INSERT INTO BOARD_COMMENT VALUES(1,'qwe',10,'get the fuck out a here',SYSDATE,4);
 INSERT INTO BOARD_COMMENT VALUES(1,'qwe',11,'반갑습니다',SYSDATE,0);
 
--- DAO TEST
-UPDATE MEMBER SET MEMBER_PW = '132' WHERE MEMBER_ID = 'asd';
+-- Dummy Data LIKED
+INSERT INTO LIKED VALUES(1,'tempId',1);
+INSERT INTO LIKED VALUES(2,'tempId',2);
+INSERT INTO LIKED VALUES(3,'qwe',1);
+INSERT INTO LIKED VALUES(4,'qwe',2);
 
-SELECT * FROM MEMBER WHERE MEMBER_ID='123';
-SELECT * FROM BOARD WHERE MEMBER_ID = 'qwe';
+-- Dummy Data REPORT
+INSERT INTO REPORT VALUES('tempId',1,1,1,SYSDATE);
+INSERT INTO REPORT VALUES('qwe',1,2,8,SYSDATE);
+INSERT INTO REPORT VALUES('123',1,3,5,SYSDATE);
+
+-- QUICK
+SELECT * FROM MEMBER;
+SELECT * FROM BOARD;
+SELECT * FROM BOARD_COMMENT;
+SELECT * FROM LIKED;
+SELECT * FROM REPORT;
+
+DESC BOARD_COMMENT;
+DESC LIKED;
+DESC REPORT;
+
+DROP TABLE BOARD;
+DROP TABLE MEMBER;
+DROP TABLE BOARD_COMMENT;
+DROP TABLE LIKED;
+DROP TABLE REPORT;
---
+++ klyWeb/src/bean/ReportBean.java
@@ -0,0 +1,43 @@
+package bean;
+
+public class ReportBean {
+	private String MEMBER_ID;
+	private int BOARD_NUM;
+	private int REPORT_NUM;
+	private int REPORT_COUNT;
+	private int REPORT_DATE;
+	public String getMEMBER_ID() {
+		return MEMBER_ID;
+	}
+	public void setMEMBER_ID(String mEMBER_ID) {
+		MEMBER_ID = mEMBER_ID;
+	}
+	public int getBOARD_NUM() {
+		return BOARD_NUM;
+	}
+	public void setBOARD_NUM(int bOARD_NUM) {
+		BOARD_NUM = bOARD_NUM;
+	}
+	public int getREPORT_NUM() {
+		return REPORT_NUM;
+	}
+	public void setREPORT_NUM(int rEPORT_NUM) {
+		REPORT_NUM = rEPORT_NUM;
+	}
+	public int getREPORT_COUNT() {
+		return REPORT_COUNT;
+	}
+	public void setREPORT_COUNT(int rEPORT_COUNT) {
+		REPORT_COUNT = rEPORT_COUNT;
+	}
+	public int getREPORT_DATE() {
+		return REPORT_DATE;
+	}
+	public void setREPORT_DATE(int rEPORT_DATE) {
+		REPORT_DATE = rEPORT_DATE;
+	}
+	
+	
+	
+	
+}
